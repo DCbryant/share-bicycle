cbee67b7dfe8d94722e0866d7039cb30f1bac716
dcbryant <dcbryant@753gmail.com>
2018-07-04 16:47:58 +0800

    配置less modlues && antd按需记载 ||可以写个博客记下来

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.less';
+import styles from './App.less';
+import {Button} from 'antd';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        demo
+      <div className={styles.App}>
+        <h3>hello world</h3>
+        <Button>点击</Button>
       </div>
     );
   }
